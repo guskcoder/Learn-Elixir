c22b6e2815eececb25a67eb522a7578e5b3187a6
Gustavo <gustavo@imaginative.company>
2022-11-30 11:09:52 -0300

    Return Elixir 2

--- gustavo/lib/gustavo.ex
+++ gustavo/lib/gustavo.ex
@@ -1,17 +1,4 @@
 defmodule Gustavo do
-  @moduledoc """
-  Documentation for `Gustavo`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Gustavo.hello()
-      :world
-
-  """
   def hello do
     :world
   end
