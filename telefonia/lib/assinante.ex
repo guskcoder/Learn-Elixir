13058e1f50d6402a8ccd1ee1f14fc6377107badf
Gustavo <gustavo@imaginative.company>
2022-12-29 13:50:07 -0300

    return studies elixir

---
+++ telefonia/lib/assinante.ex
@@ -0,0 +1,21 @@
+defmodule Assinante do
+  defstruct nome: nil, numero: nil, cpf: nil, plano: nil
+
+  @assinantes %{:prepago => "pre.txt", :pospago => "pos.txt"}
+
+  def cadastrar(nome, numero, cpf, plano \\ :prepago) do
+    read(plano) ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}]
+    |> :erlang.term_to_binary()
+    |> write(plano)
+  end
+
+  defp write(lista_assinantes, plano) do
+    File.write!(@assinantes[plano], lista_assinantes)
+  end
+
+  def read(plano) do
+    {:ok, assinantes} = File.read(@assinantes[plano])
+    assinantes
+    |> :erlang.binary_to_term()
+  end
+end
